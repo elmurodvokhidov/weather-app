373ca8fca0a4378b0f17269155a17bf4e73fdf6a
Elmurod Vokhidov <elmurodvkh@gmail.com>
2023-09-03 21:18:27 +0500

    location access

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import Loader from './components/Loader';
 import { useEffect, useState } from 'react';
 import Weather from './components/Weather';
+import * as Location from 'expo-location';
+import axios from 'axios';
+
+const API_KEY = "db53a00eee2da0ebe4ab04359ba97ddf";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+
+  const getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    setIsLoading(false);
+    console.log(data);
+  }
+
+  const getLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert("Permission to access location was denied");
+        return
+      }
+
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({});
+      getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Something's went wrong here!");
+    }
+  };
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, [])
+    getLocation();
+  }, []);
 
 
   return (
